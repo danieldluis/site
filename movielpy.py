94b3c002512958dd20f29e989dce478ffae73108
danieldluis <danielpytao@gmail.com>
2026-09-04 13:56:58 +0100

    Update movielpy.py

--- movielpy.py
+++ movielpy.py
@@ -5,3 +5,9 @@ from moviepy import VideoFileClip, vfx
 clip = VideoFileClip(r"C:\Users\Daniel\Music\video1.mp4").subclipped(27.5, 32.5).resized(width=480).with_effects([vfx.MultiplySpeed(4)])
 
 clip.write_gif(r"C:\Users\Daniel\Music\output.gif")
+
+
+#Parameters:
+#subclipped(start point, end point) - Dividing Clip
+#resized(width=480) - Resize Window of Clip
+#with_effects([vfx.MultiplySpeed(4)]) - How many times the Normal Speed
